e1100d577e4a619e7b81d11c21ea58822025dd06
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-14 19:24:42 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+
+end
+
+def position_taken?(board, index)
+  !((board[index] == "") || (board[index] == " ") || (board[index]) == nil)
+end
