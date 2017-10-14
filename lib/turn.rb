557ae0524aff8695e6fb3bf7838f3b2f702353cc
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-14 19:26:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,10 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input.to_i -= 1
+end
+
 def valid_move?(board, index)
   (!position_taken?(board, index) && index.between?(0, 8))
 end
