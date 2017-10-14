985d737c42dcb8b45d1852bc4dd837c7e1fc1ff9
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-14 19:29:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,7 @@ end
 def position_taken?(board, index)
   !((board[index] == "") || (board[index] == " ") || (board[index]) == nil)
 end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
